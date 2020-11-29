68deec612b4ad8098e62f2ba6c6383cbfa5abddd
TSGames1 <anfragen@tsgames.de>
2020-11-29 17:55:58 +0100

    init angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,70 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'santa-dice';
+  static API_URL = 'http://127.0.0.1/dice/api.php';
+  name: string;
+  isAdded = false;
+  dices: Dice[];
+  isDicing = false;
+  constructor(private http: HttpClient){
+      this.name = localStorage.getItem("name");
+      setInterval(() => this.updateList(), 20000);
+      this.updateList();
+  }
+  async updateList() {
+    const newDices = await this.http.get<Dice[]>(AppComponent.API_URL).toPromise();
+    if(this.isAdded){
+      let data = newDices?.filter((d) => d.name === this.name);
+      if(this.isDicing) {
+        data = this.dices.filter((d) => d.name === this.name)
+      }
+      this.dices = data.concat(newDices.filter((d) => d.name !== this.name));
+
+    } else {
+      this.dices = newDices;
+    }
+
+  }
+  highest() {
+    return this.dices?.reduce((d1,d2) => d1.number > d2.number ? d1 : d2);
+  }
+  async reset(mode: 'reset' | 'delete'){
+    await this.http.get<Dice[]>(AppComponent.API_URL + '?action=' + mode).toPromise();
+    this.updateList();
+  }
+  async addName(){
+    this.isAdded = true;
+    localStorage.setItem("name", this.name);
+    // only add if not in list
+    if(!this.getMyDice()){
+      await this.http.get<Dice[]>(AppComponent.API_URL + '?action=put&name=' + this.name).toPromise();
+      console.log("add");
+    }
+    this.updateList();
+  }
+  getMyDice(){
+    return this.dices?.filter((d) => d.name === this.name)[0];
+  }
+  diceNumber(){
+    const oldNumber = this.getMyDice().number;
+    do{
+      this.getMyDice().number = Math.floor(Math.random() * 6) + 1;
+    }while(this.getMyDice().number === oldNumber);
+  }
+  startDice(){
+    this.isDicing = true;
+    const int = setInterval(() => this.diceNumber(), 200);
+    this.diceNumber();
+    setTimeout(async () => {
+      clearInterval(int);
+      this.isDicing = false;
+      await this.http.get<Dice[]>(AppComponent.API_URL + '?action=put&name=' + this.name + '&number=' + this.getMyDice().number).toPromise();
+      this.updateList();
+    }, 5000);
+  }
+}
+
+export interface Dice{
+  name: string;
+  number: number;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
   ],
-  providers: [],
+  providers: [HttpClientModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
